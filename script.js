86c421226add786615c382bed6c77a60d1d55fe9
Adyzero <radikasbi.psz@gmail.com>
2026-03-31 09:54:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ window.addEventListener("click", function(e) {
 });
 
 // ==========================
-// CLICK FILE
+// CLICK FILE (OPEN IN MODAL)
 document.addEventListener("click", function(e) {
   let btn = e.target.closest(".view-file");
 
@@ -48,7 +48,7 @@ document.addEventListener("click", function(e) {
       return;
     }
 
-    window.open(url, "_blank");
+    openModal(url); // ✅ use modal instead
   }
 });
 
